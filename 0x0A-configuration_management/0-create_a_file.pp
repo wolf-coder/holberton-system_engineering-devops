ac5b5426fa3bd389f546fdb57928810cbe97356f
wolf-coder <cuore.fakhri@gmail.com>
2020-08-03 21:33:04 +0100

    fixing style

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,9 @@
 # create a file in /tmp
-file { "tmp":
-  path => "/tmp/holberton",
-  ensure => "present",
-  mode => "0744",
-  owner => "www-data",
-  group => "www-data",
+file { "holberton":
+  path    => "/tmp/holberton",
+  ensure  => "present",
+  mode    => "0744",
+  owner   => "www-data",
+  group   => "www-data",
   content => "I love Puppet",
 }
