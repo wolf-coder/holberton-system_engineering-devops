0478fbc1f1c9f07019d53cf8bbfda434e685c905
wolf-coder <cuore.fakhri@gmail.com>
2020-08-04 16:38:41 +0100

    Fix

--- 0x0B-ssh/4-puppet_ssh_config.pp
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -1,6 +1,5 @@
 #Configuration with pupppet
 exec { 'Turn off passwd auth':
-  command => 'sed -i \'s/PasswordAuthentication no/PasswordAuthentication yes/g\' /tmp/sshd_config && 
-echo -e "\tIdentityFile ~/.ssh/holberton" >> /etc/ssh/ssh_config',
+#  command => 'sed -i \'s/PasswordAuthentication no/PasswordAuthentication yes/g\' /tmp/sshd_config ;echo -e "\tIdentityFile ~/.ssh/holberton" >> /etc/ssh/ssh_config',
   path    => '/bin/'
 }
