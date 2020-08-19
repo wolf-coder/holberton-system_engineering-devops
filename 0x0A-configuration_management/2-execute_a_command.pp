133020353360c2ef161f0835f321c605afe274e7
wolf-coder <cuore.fakhri@gmail.com>
2020-08-19 17:40:55 +0100

    Puppet Config

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,5 +1,10 @@
 #exec a command : killing a process
-exec { 'kidllmenow':
+exec { 'killmenow':
   command  => 'pkill killmenow',
   provider => 'shell',
 }
+
+
+------------------------------------------------------->
+(insert-image (create-image "/home/cuore-pc/Programming/Holberton/Projects/holberton-system_engineering-devops/0x0A-configuration_management/Testing/Revise/Pictures/03.png"))
+
