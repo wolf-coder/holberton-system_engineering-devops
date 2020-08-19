41da383d3f6b2d8492b8ff218b0465927a933943
wolf-coder <cuore.fakhri@gmail.com>
2020-08-19 18:27:37 +0100

    Fixing

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,14 +1,16 @@
-exec{ 'apt-get update':
-  command => '/usr/bin/apt-get update',
+exec{ 'Updating Repostories':
+  provider => shell,
+  command => 'sudo apt-get update',
+  before => Exec['Installing nginx'],
 }
-package{ 'nginx':
-  ensure => 'installed',
-  require => Exec['apt-get update'],
+exec{ 'Installing nginx':
+  provider => shell,
+  command => 'sudo apt-get -y install nginx',
   before => Exec['add_header'],
 }
 exec { 'add_header':
   provider    => shell,
-  command     => 'sed -i "44i  add_header X-Served-By  ""\$HOSTNAME"";" /etc/nginx/sites-available/default',
+  command     => 'sudo sed -i "44i  add_header X-Served-By  ""\$HOSTNAME"";" /etc/nginx/sites-available/default',
   before      => Exec['Nginx Restaring'],
 }
 exec { 'Nginx Restaring':
