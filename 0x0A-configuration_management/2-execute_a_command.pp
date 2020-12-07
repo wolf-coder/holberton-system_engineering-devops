bce44ab6ecbcf61148fe2aef15b5070d4813a6bf
wolf-coder <cuore.fakhri@gmail.com>
2020-12-07 23:17:44 +0100

    Ficxing

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -3,7 +3,3 @@ exec { 'killmenow':
   command  => 'pkill killmenow',
   provider => 'shell',
 }
-
-
-# ------------------------------------------------------->
-# (insert-image (create-image "/home/cuore-pc/Programming/Holberton/Projects/holberton-system_engineering-devops/0x0A-configuration_management/Testing/Revise/Pictures/03.png"))
