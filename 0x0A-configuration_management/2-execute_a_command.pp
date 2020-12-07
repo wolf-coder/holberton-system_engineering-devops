f49dedc12a50059478482b0cdf3b94b3b2c1ce2b
wolf-coder <cuore.fakhri@gmail.com>
2020-12-07 23:16:06 +0100

    Fix

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -5,6 +5,5 @@ exec { 'killmenow':
 }
 
 
-------------------------------------------------------->
-(insert-image (create-image "/home/cuore-pc/Programming/Holberton/Projects/holberton-system_engineering-devops/0x0A-configuration_management/Testing/Revise/Pictures/03.png"))
-
+# ------------------------------------------------------->
+# (insert-image (create-image "/home/cuore-pc/Programming/Holberton/Projects/holberton-system_engineering-devops/0x0A-configuration_management/Testing/Revise/Pictures/03.png"))
