ddc5f440559d9621ff6a426e6eed9c7c608021cb
wolf-coder <cuore.fakhri@gmail.com>
2020-12-21 23:38:40 +0100

    Puppet

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,19 +1,18 @@
-#Cinfiguring with puppet
+#Configuring with puppet
 
 exec{ 'Updating Repostories':
   provider => shell,
   command  => 'sudo apt-get -y update',
-  before   => Exec['Installing nginx'],
 }
-exec{ 'Installing nginx':
-  provider => shell,
-  command  => 'sudo apt-get -y install nginx',
+package{ 'nginx':
+  ensure   => 'installed',
+  require => Exec['Updating Repostories'],
   before   => Exec['add_header'],
 }
 exec { 'add_header':
   provider    => shell,
   environment => ["HOSTNAME=${hostname}"],
-    command   => 'sudo sed -i "s/include \/etc\/nginx\/sites-enabled\/\*;/include \/etc\/nginx\/sites-enabled\/\*;\n\tadd_header X-Served-By \"$HOSTNAME\";/" /etc/nginx/nginx.conf',
+  command     => 'sed -i "44i  add_header X-Served-By \"$HOSTNAME\";" /etc/nginx/sites-available/default',
   before      => Exec['Nginx Restaring'],
 }
 exec { 'Nginx Restaring':
