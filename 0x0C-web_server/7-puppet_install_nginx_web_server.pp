ae5f989c2fb852a7d7515449c860f3bb13e667a7
wolf-coder <cuore.fakhri@gmail.com>
2021-01-27 01:27:36 +0100

    configure with nginx

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,39 @@
+#updating system
+exec{ 'Updating Repostories':
+  provider => shell,
+  command  => 'sudo apt-get -y update',
+}
+#installing nginx
+package{ 'nginx':
+  ensure  => 'installed',
+  require => Exec['Updating Repostories'],
+  before  => Exec['Nginx Restaring'],
+}
+# creating a file in /tmp
+file { 'index.nginx-debian.html':
+  path    => '/var/www/html/index.nginx-debian.html',
+  owner   => root,
+  content => 'Holberton School',
+  before  => Package['nginx'],
+}
+# creating defaul block with requirements
+file { 'default':
+  path    => '/etc/nginx/sites-available/default',
+  owner   => root,
+  content => 'server {
+       listen 80 default_server;
+       listen [::]:80 default_server;
+       root /var/www/html ;
+       index index.html index.htm index.nginx-debian.html;
+       server_name _;
+       location / {
+       		try_files \$uri \$uri/ =301;
+	}
+}',
+  require => File['index.nginx-debian.html'],
+}
+#restaring nginx
+exec { 'Nginx Restaring':
+  provider => shell,
+  command  => 'sudo service nginx restart',
+}
