8b3ae1ab432bff4833905ab222eb72dd3473f7a4
wolf-coder <cuore.fakhri@gmail.com>
2021-07-07 15:40:41 +0100

    Now you work or i will suicide

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,5 @@
 # incorrect file name
 exec{ 'fix-wordpress':
     command => 'sed -i \'s/class-wp-locale.phpp/class-wp-locale.php/g\' /var/www/html/wp-settings.php',
-    path    => '/usr/local/bin/',
+    path    => '/usr/local/bin/:/bin/',
 }
