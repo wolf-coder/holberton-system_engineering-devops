b480e8c74189b14a0a5bcbd4e2d078edc1c4fe17
wolf-coder <cuore.fakhri@gmail.com>
2021-07-08 19:59:52 +0100

    Now you got to work

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -1,51 +1,50 @@
 #!/usr/bin/python3
 """
-recursive function that queries the Reddit API.
+recursive function.
 """
-import pprint
 import re
 import requests
+headers = {'user-agent': 'ubuntu:hbtn:v1.0\
+ (by /u/Brandixitor)'}
 
 
-def count_words(subreddit, word_list, hot_list=[], after=None):
-    '''function count_words : Get ALL hot posts'''
-    headers = {'User-agent': 'Unix:0-subs:v1'}
-    params = {'limit': 100}
-    if isinstance(after, str):
-        if after != "STOP":
-            params['after'] = after
-        else:
-            return Display_Results(word_list, hot_list)
-
-    response = requests.get('http://reddit.com/r/{}/hot.json'.
-                            format(subreddit),
-                            headers=headers,
-                            params=params)
-    if response.status_code != 200:
-        return None
-    data = response.json().get('data', {})
-    after = data.get('after', 'STOP')
-    if not after:
-        after = "STOP"
-    hot_list = hot_list + [post.get('data', {}).get('title')
-                           for post in data.get('children', [])]
-    return count_words(subreddit, word_list, hot_list, after)
-
-
-def Display_Results(word_list, hot_list):
-    '''
-    function Display_Results :Prints request results
-    '''
-    count = {}
-    for word in word_list:
-        count[word] = 0
-    for title in hot_list:
-        for word in word_list:
-            count[word] = count[word] +\
-             len(re.findall(r'(?:^| ){}(?:$| )'.format(word), title, re.I))
-
-    count = {k: v for k, v in count.items() if v > 0}
-    words = sorted(list(count.keys()))
-    for word in sorted(words,
-                       reverse=True, key=lambda k: count[k]):
-        print("{}: {}".format(word, count[word]))
+def count_words(subreddit, word_list, after='', occurs={}):
+    """
+    Write a recursive function that queries the Reddit API, parses the
+    title of all hot articles, and prints a sorted count of given
+    keywords (case-insensitive, delimited by spaces.
+    Javascript should count as javascript, but java should not).
+    """
+    url = 'https://api.reddit.com/r/{}?limit=100&after={}'.
+    format(subreddit, after)
+    response = requests.get(url, headers=headers)
+    try:
+        data = response.json()
+    except Exception:
+        return
+    if (str(response.status_code) == '404'):
+        return
+    Data_Len = len(data['data']['children'])
+    if (Data_Len is 0):
+        return
+    for i in range(0, Data_Len):
+        try:
+            GetTitle = data['data']['children'][i]['data']['title']
+            for a in word_list:
+                try:
+                    occurs[a]
+                except KeyError:
+                    occurs[a] = 0
+                finally:
+                    occurs[a] += re.subn(r'(?i)(?<!\S)\b{}\b(?!\S)'.format(a),
+                                         '', GetTitle)[1]
+        except Exception:
+            pass
+    ValNext = data['data']['after']
+    if (ValNext is not None):
+        return count_words(subreddit, word_list, ValNext, occurs)
+    else:
+        for key in sorted(occurs, key=lambda k: (-occurs[k], k)):
+            if (occurs[key] > 0):
+                print("{}: {}".format(key, occurs[key]))
+        return
