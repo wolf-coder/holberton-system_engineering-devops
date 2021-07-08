aba7c15ea9e6e25a3afc67191381102d6bf817bc
wolf-coder <cuore.fakhri@gmail.com>
2021-07-08 17:56:33 +0100

    fix

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,10 +1,10 @@
 exec {'hard':
-path     => ['/bin/'],
+path     => ['/bin/', '/sbin/', '/usr/bin/', '/usr/sbin/'],
 command  => "sudo sed -i 's/holberton hard nofile 5/holberton hard nofile 65536/g' /etc/security/limits.conf; /sbin/sysctl -p",
 provider => 'shell',
 }
 exec {'soft':
-path     => ['/bin/'],
+path     => ['/bin/', '/sbin/', '/usr/bin/', '/usr/sbin/'],
 command  => "sudo sed -i 's/holberton soft nofile 4/holberton soft nofile 65536/g' /etc/security/limits.conf; /sbin/sysctl -p",
 provider => 'shell',
 }
